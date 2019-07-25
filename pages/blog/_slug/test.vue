a820bdb814e20ac6becb85a98966b4ac88162962
Peyman Eskandari <peyman.github@gmail.com>
2019-07-25 11:24:30 +0430

    Initial commit

---
+++ pages/blog/_slug/test.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+
+    <div v-html="Content"></div>
+
+  </section>
+</template>
+
+<script>
+  import Content from '~/md/hello.md'
+
+  export default {
+    name: "hello",
+    layout: "default",
+    computed: {
+      Content() {
+        return Content
+      }
+    }
+  }
+</script>
+
+<style>
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+                 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+</style>
